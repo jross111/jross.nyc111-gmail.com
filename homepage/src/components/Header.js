f85068216c1bf6161eca2e9f717c403eea29f832
jross111 <jross.nyc111@gmail.com>
2020-03-27 23:10:34 -0400

    :tada:

---
+++ homepage/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const Header = () => {
+	return (
+		<div>
+			<div className='header'>
+				<div className='container clearfix'>
+					<h1 class='name'>James Ross</h1>
+					<h2 class='desc'>Software Developer</h2>
+				</div>
+			</div>
+		</div>
+	);
+};
