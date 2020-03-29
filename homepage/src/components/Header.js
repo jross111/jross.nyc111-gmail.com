e0e2516738c3d71dcce476773900b9669b9a2025
jross111 <jross.nyc111@gmail.com>
2020-03-29 13:29:38 -0400

    :checkered_flag:

--- homepage/src/components/Header.js
+++ homepage/src/components/Header.js
@@ -1,12 +1,29 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 
 export const Header = () => {
 	return (
 		<div>
 			<div className='header'>
 				<div className='container clearfix'>
-					<h1 class='name'>James Ross</h1>
-					<h2 class='desc'>Software Developer</h2>
+					<img
+						className='profile-image img-fluid float-left'
+						src='https://i.gyazo.com/71bca8e5caa93de8b247fc5af58e94a7.jpg'
+						alt='James Lee'
+					/>
+					<div class='profile-content float-left'>
+						<h1 className='name'>James Ross</h1>
+						<h2 className='desc'>Software Developer</h2>
+						<ul className='social list-inline'>
+							<li className='list-inline-item'>
+								<FontAwesomeIcon icon={faCoffee} />
+							</li>
+							<li className='list-inline-item'>
+								<FontAwesomeIcon icon={faCoffee} />
+							</li>
+						</ul>
+					</div>
 				</div>
 			</div>
 		</div>
